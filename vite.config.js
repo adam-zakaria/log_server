6a4c4e1c4723b392b3877d6be5b6e0065a58f7c4
Adam Zakaria <adam.p.zakaria@gmail.com>
2024-12-03 01:48:16 -0500

    Add files

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [],
+    server: {
+      port: process.env.PORT || 5173, // Default to 5173 if PORT is not set
+      host: '0.0.0.0',
+      hmr: mode === 'aws_dev', // Enable HMR only in 'aws_dev' mode
+    },
+  };
+});
